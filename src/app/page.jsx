8d4efc109e143cc8d2e89f2205330961d3309c7a
Stefan C <stefan@nexis.network>
2023-12-09 09:39:58 -0500

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,6 @@
-import Layout from 'components/shared/layout';
 import SEO_DATA from 'constants/seo-data';
 import getMetadata from 'utils/get-metadata';
+
 import AIPage from './ai/page';
 
 export const metadata = getMetadata(SEO_DATA.index);
